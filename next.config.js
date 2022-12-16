61b0d929743eb6f95edbaa1915aa5966f35eb0a3
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2022-12-16 13:03:52 +0000

    Swap commonjs to module

--- next.config.js
+++ next.config.js
@@ -1,30 +1,24 @@
 /** @type {import('next').NextConfig} */
-const { remarkCodeHike } = require("@code-hike/mdx");
-const theme = require("shiki/themes/one-dark-pro.json");
+import { remarkCodeHike } from '@code-hike/mdx';
+import theme from 'shiki/themes/monokai.json' assert { type: 'json' };
+import nextMDX from '@next/mdx';
 
-const withMDX = require("@next/mdx")({
+const withMDX = nextMDX({
     extension: /\.mdx?$/,
     options: {
         // If you use remark-gfm, you'll need to use next.config.mjs
         // as the package is ESM only
         // https://github.com/remarkjs/remark-gfm#install
-        remarkPlugins: [
-            [
-                remarkCodeHike,
-                {
-                    theme,
-                    showCopyButton: true,
-                },
-            ],
-        ],
+        remarkPlugins: [[remarkCodeHike, { theme }]],
         rehypePlugins: [],
         // If you use `MDXProvider`, uncomment the following line.
         // providerImportSource: "@mdx-js/react",
     },
 });
-module.exports = withMDX({
+
+export default withMDX({
     // Append the default value with md extensions
-    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     experimental: {
         appDir: true,
     },
