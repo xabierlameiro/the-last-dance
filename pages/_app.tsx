e1d8c3354b74b9bbd76e84f08db2f386d7ef7b21
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2022-12-16 09:49:34 +0000

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
 import "@code-hike/mdx/dist/index.css";
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />;
+            <Analytics />
+        </>
+    );
 }
