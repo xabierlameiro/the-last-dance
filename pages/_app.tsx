b0a656294b4410631b3b455fa34da781c95c80c8
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2022-12-23 12:39:52 +0000

    Change in master

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import '../styles/ch-styles.css';
 import '../styles/globals.css';
 import '@xabierlameiro/code-hike/dist/index.css';
 import { Analytics } from '@vercel/analytics/react';
-import { DialogProvider } from '@/context/dialog';
 import type { AppProps } from 'next/app';
+import { DialogProvider } from '@/context/dialog';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
