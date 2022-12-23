fa6bf9ca99e648b4818bd3b870782ba5e66e0981
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2022-12-23 09:08:16 +0000

    New token

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import '../styles/ch-styles.css';
 import '../styles/globals.css';
 import '@xabierlameiro/code-hike/dist/index.css';
 import { DialogProvider } from '@/context/dialog';
-import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
