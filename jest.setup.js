356fbc70cbf5214a255b1eb645bc230a55145a58
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2022-12-28 23:26:22 +0000

    Add new jest configuration to try to skip error on production

--- jest.setup.js
+++ jest.setup.js
@@ -7,5 +7,16 @@ const AllTheProviders = ({ children }) => {
 const customRender = (ui, options) =>
     render(ui, { wrapper: AllTheProviders, ...options });
 
+// hide error messages about act() being unsupported in production build
+const ignoredErrors = [
+    /act(...) is not supported in production builds of React, and might not behave as expected./,
+];
+const consoleError = global.console.error;
+global.console.error = (...args) => {
+    if (ignoredErrors.some((el) => el.test(args[0]))) {
+        return consoleError(...args);
+    }
+};
+
 export * from '@testing-library/react';
 export { customRender as render };
