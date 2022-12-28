1cb7aa03f51881ba7996071e91e4a5e8eab7b9b7
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2022-12-28 23:35:54 +0000

    Remove last configuration

--- jest.setup.js
+++ jest.setup.js
@@ -7,16 +7,5 @@ const AllTheProviders = ({ children }) => {
 const customRender = (ui, options) =>
     render(ui, { wrapper: AllTheProviders, ...options });
 
-// hide error messages about act() being unsupported in production build
-const ignoredErrors = [
-    /act(...) is not supported in production builds of React./,
-];
-const consoleError = global.console.error;
-global.console.error = (...args) => {
-    if (ignoredErrors.some((el) => el.test(args[0]))) {
-        return consoleError(...args);
-    }
-};
-
 export * from '@testing-library/react';
 export { customRender as render };
