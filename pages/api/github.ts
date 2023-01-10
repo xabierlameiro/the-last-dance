67bf4e19fc6358c10fb1c84da5c140b12c06c81b
xabier.lameirocardama <xabier.lameiro@gmail.com>
2023-01-10 20:44:08 +0000

    Add github star crud

--- pages/api/github.ts
+++ pages/api/github.ts
@@ -3,45 +3,55 @@ import { Octokit } from 'octokit';
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
+const REPOSITORY = {
+    owner: 'xabierlameiro',
+    repo: 'the-last-dance',
+};
+
 /**
  * @description This function is used to get the total number of stars for a given repository. It uses the GitHub and
  * put a star on it.
  *
+ * @example
+ *     GET /api/github
+ *     GET /api/github?starred=true
+ *     POST /api/github
+ *     DELETE /api/github
+ *
  * @param {NextApiRequest} req
- * @param {NextApiResponse<Data>} res
- * @returns {Promise<{
- *     error?: string;
- *     total?: number;
- * }>}
+ * @param {NextApiResponse} res
+ * @returns {Promise<void>}
  */
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
-        try {
-            const {
-                data: { stargazers_count },
-            } = await octokit.rest.repos.get({
-                owner: 'xabierlameiro',
-                repo: 'the-last-dance',
-            });
+    const { starred } = req.query;
 
-            res.status(200).json(stargazers_count);
-        } catch (err: any) {
-            res.status(500).json({ statusCode: 500, message: err.message });
+    if (req.method === 'GET') {
+        if (!starred) {
+            try {
+                const {
+                    data: { stargazers_count },
+                } = await octokit.rest.repos.get(REPOSITORY);
+                res.status(200).json(stargazers_count);
+            } catch (err: any) {
+                res.status(500).json({ statusCode: 500, message: err.message });
+            }
+        } else {
+            try {
+                await octokit.rest.activity.checkRepoIsStarredByAuthenticatedUser(REPOSITORY);
+                res.status(200).json(true);
+            } catch (err: any) {
+                if (err.status === 404) {
+                    res.status(200).json(false);
+                } else {
+                    res.status(500).json({ statusCode: 500, message: err.message });
+                }
+            }
         }
     }
 
     if (req.method === 'POST') {
         try {
-            await octokit.rest.activity.starRepoForAuthenticatedUser({
-                owner: 'xabierlameiro',
-                repo: 'the-last-dance',
-                request: {
-                    mediaType: {
-                        previews: ['squirrel'],
-                    },
-                },
-            });
-
+            await octokit.rest.activity.starRepoForAuthenticatedUser(REPOSITORY);
             res.status(200).json({ message: 'starred' });
         } catch (err: any) {
             res.status(500).json({ statusCode: 500, message: err.message });
@@ -50,16 +60,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     if (req.method === 'DELETE') {
         try {
-            await octokit.rest.activity.unstarRepoForAuthenticatedUser({
-                owner: 'xabierlameiro',
-                repo: 'the-last-dance',
-                request: {
-                    mediaType: {
-                        previews: ['squirrel'],
-                    },
-                },
-            });
-
+            await octokit.rest.activity.unstarRepoForAuthenticatedUser(REPOSITORY);
             res.status(200).json({ message: 'unstarred' });
         } catch (err: any) {
             res.status(500).json({ statusCode: 500, message: err.message });
--- src/components/Blog/StarCounter/index.tsx
+++ src/components/Blog/StarCounter/index.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import styles from './starCounter.module.css';
-import { AiOutlineStar } from 'react-icons/ai';
+import { AiOutlineStar, AiFillStar } from 'react-icons/ai';
 import { FaSpinner } from 'react-icons/fa';
 import { RxCross2 } from 'react-icons/rx';
 
 type Props = {
     children?: React.ReactNode;
+    hasStar?: boolean;
+    setRefresh?: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const Container = ({ children }: Props) => {
+const Container = ({ children, hasStar, setRefresh }: Props) => {
+    const starHandler = async () => {
+        try {
+            if (hasStar) {
+                await fetch('/api/github', { method: 'DELETE' });
+            } else {
+                await fetch('/api/github', { method: 'POST' });
+            }
+        } catch (e) {
+            console.log(e);
+        }
+        setRefresh?.((value: boolean) => !value);
+    };
+
     return (
         <div className={styles.stars}>
-            <AiOutlineStar title="Stars from github" />
+            {hasStar ? (
+                <AiFillStar onClick={starHandler} className={styles.starred} title="Starred" />
+            ) : (
+                <AiOutlineStar onClick={starHandler} title="Stars from github" />
+            )}
             {children}
         </div>
     );
@@ -19,17 +38,28 @@ const Container = ({ children }: Props) => {
 
 const StarCounter = () => {
     const [stars, setStars] = React.useState(0);
+    const [hasStar, setHasStar] = React.useState(false);
+    const [refresh, setRefresh] = React.useState(false);
 
     React.useEffect(() => {
         (async () => {
             try {
                 const stars = await fetch('/api/github').then((res) => res.json());
                 setStars(stars);
+                const hadStar = await fetch('/api/github?starred=true').then((res) => res.json());
+                setHasStar(hadStar);
             } catch (e) {
                 setStars(-1);
             }
         })();
-    }, []);
+    }, [refresh]);
+
+    if (stars === -2)
+        return (
+            <Container>
+                <AiFillStar className={styles.starred} title="Starred" />
+            </Container>
+        );
 
     if (stars === -1)
         return (
@@ -46,7 +76,7 @@ const StarCounter = () => {
         );
 
     return (
-        <Container>
+        <Container hasStar={hasStar} setRefresh={setRefresh}>
             <span>{stars}</span>
         </Container>
     );
