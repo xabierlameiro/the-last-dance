6a90b4cb61e7e4d14252d4d90ae2b4bdda565802
xabierlameiro.com <xabier.lameiro@gmail.com>
2023-01-14 17:11:04 +0000

    Display block

--- src/components/GoogleAdsense/index.tsx
+++ src/components/GoogleAdsense/index.tsx
@@ -10,7 +10,7 @@ type Props = {
 const GoogleAdsense = ({ client = 'ca-pub-3537017956623483', slot, horizontal }: Props) => {
     return (
         <ins
-            className={clx('adsbygoogle', horizontal ? styles.horizontal : '')}
+            className={clx('adsbygoogle', horizontal ? styles.horizontal : styles.block)}
             data-ad-client={client}
             data-ad-slot={slot}
             data-ad-format="auto"
