e674b326e6518aa93a76c1ef966f605657d432f0
xabierlameiro.com <xabier.lameiro@gmail.com>
2023-01-16 17:39:49 +0000

    Changes in script

--- custom-docs.js
+++ custom-docs.js
@@ -16,6 +16,8 @@ glob('public/docs/*.?(html|css|ts.html)', function (err, files) {
 
             let replaced = data.replace(/<title>(.*)<\/title>/, '<title>Docs by Xabier Lameiro</title>');
 
+            replaced = replaced.replace(/href="docs\/index.html"/g, 'href="/doc');
+
             if (path === 'public/docs/index.html') {
                 replaced = replaced.replace(/script src="/g, 'script src="docs/');
                 replaced = replaced.replace(/rel="stylesheet" href="/g, 'rel="stylesheet" href="docs/');
