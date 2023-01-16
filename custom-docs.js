ada16163e07d50174655e618aa7b0a03f9e8e376
xabierlameiro.com <xabier.lameiro@gmail.com>
2023-01-16 17:50:51 +0000

    Remove code

--- custom-docs.js
+++ custom-docs.js
@@ -18,11 +18,6 @@ glob('public/docs/*.?(html|css|ts.html)', function (err, files) {
 
             replaced = replaced.replace(/href="docs\/index.html"/g, 'href="/doc');
 
-            if (path === 'public/docs/index.html') {
-                replaced = replaced.replace(/script src="/g, 'script src="docs/');
-                replaced = replaced.replace(/rel="stylesheet" href="/g, 'rel="stylesheet" href="docs/');
-            }
-
             writeFile(path, replaced, 'utf-8', function (err, a) {
                 if (err) {
                     console.log('err', err);
