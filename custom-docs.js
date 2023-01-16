f3fa25d887a984c791c08cf78470f04fcb5ae293
xabierlameiro.com <xabier.lameiro@gmail.com>
2023-01-16 18:15:48 +0000

    Remove code

--- custom-docs.js
+++ custom-docs.js
@@ -16,7 +16,12 @@ glob('public/docs/*.?(html|css|ts.html)', function (err, files) {
 
             let replaced = data.replace(/<title>(.*)<\/title>/, '<title>Docs by Xabier Lameiro</title>');
 
-            replaced = replaced.replace(/href="docs\/index.html"/g, 'href="/doc');
+            if (path === 'public/docs/index.html') {
+                replaced = replaced.replace(/src="/g, 'src="docs/');
+                replaced = replaced.replace(/href="/g, 'href="docs/');
+                // replaced = replaced.replace(/rel="stylesheet" href="/g, 'rel="stylesheet" href="docs/');
+                replaced = replaced.replace(/url\('/g, "url('docs/");
+            }
 
             writeFile(path, replaced, 'utf-8', function (err, a) {
                 if (err) {
