9f513ffe093984b1507a19a7984545ebf3b33f38
xabierlameiro.com <xabier.lameiro@gmail.com>
2023-01-31 10:27:46 +0000

    Remove legal docs from sitemap

--- src/helpers/fileWritter.ts
+++ src/helpers/fileWritter.ts
@@ -66,12 +66,6 @@ export const createSiteMap = (
             };
         });
 
-    sitemap = sitemap.concat([
-        `${process.env.NEXT_PUBLIC_DOMAIN}/legal/cookies-policy`,
-        `${process.env.NEXT_PUBLIC_DOMAIN}/legal/legal-notice`,
-        `${process.env.NEXT_PUBLIC_DOMAIN}/legal/privacy-policy`,
-    ]);
-
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${sitemap
