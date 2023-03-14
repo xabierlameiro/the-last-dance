ada86d62a3bf720629b665b08fdc3ca55d8d8341
xabierlameiro.com <xabier.lameiro@gmail.com>
2023-03-14 22:36:42 +0000

    Playwright workflow

---
+++ playwright.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from '@playwright/test';
+export default defineConfig({
+    use: {
+        headless: false,
+        viewport: { width: 1280, height: 720 },
+        ignoreHTTPSErrors: true,
+        video: 'on-first-retry',
+        baseURL: 'http://localhost:3000',
+    },
+    testDir: './e2e',
+});
