752e25d84aeef56d5bdc00bc4d970f34c5acab70
xabierlameiro.com <xabier.lameiro@gmail.com>
2023-03-15 00:33:25 +0000

    Changes on test end to end

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,6 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('should navigate to the about page', async ({ page }) => {
+test('should navigate to the landing page', async ({ page }) => {
     // Just navigate to landing page
     await page.goto('/');
+    // Check the title
+    await expect(page).toHaveTitle(/Front end developer, microcomputing and networks Technician/);
+    // check if the header, main and footer are visible
+    await page.locator('[data-testid="header"]').isVisible();
+    await page.locator('[data-testid="main"]').isVisible();
+    await page.locator('[data-testid="footer"]').isVisible();
+
+    // TODO : Close the cookies modal
+    // TODO : Close and open the modal
+    // TODO : Navigate in the modal
+    // TODO : Open links to the social networks and reports
+    // TODO : Check the tooltips
+    // TODO : Open the weather widget and news, scroll and close
+    // ...
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from '@playwright/test';
 export default defineConfig({
     use: {
-        headless: true,
+        headless: false,
         viewport: { width: 1280, height: 720 },
         ignoreHTTPSErrors: true,
         video: 'on-first-retry',
-        baseURL: 'http://localhost:3000',
+        baseURL: 'https://pre.xabierlameiro.com',
     },
     testDir: './e2e',
 });
